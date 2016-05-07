6e2cf73d3aa85af0dbfc3e71f897c12527265c0d
aleksei <alekseik1@yandex.ru>
2016-05-07 14:36:39 +0300

    Перенос полностью работает и прописывает в addon.d, для каждого файла - свой скрипт. Ура! Дальше - больше: нужно сделать предложение сделать ребут после переноса, добавить сортировку списка, поиск по списку и решить вопрос с переворачиванием экрана.

--- app/src/main/java/com/alex/testapp2/RootUtils.java
+++ app/src/main/java/com/alex/testapp2/RootUtils.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import eu.chainfire.libsuperuser.Shell;
 
 /**
@@ -19,6 +20,7 @@ public class RootUtils {
     public static void moveToSystem(ApplicationInfo app, Context context) {
         StringBuilder sb = new StringBuilder();
         String appDir;
+        String scriptName = app.loadLabel(context.getPackageManager()).toString().toLowerCase().replace(" ", "");
         String appOldDir;
         appOldDir = sb.append("/data").append(app.sourceDir.substring(app.sourceDir.indexOf("/", 1))).substring(0, sb.indexOf("base.apk"));
         sb.delete(0, sb.length());
@@ -44,11 +46,9 @@ public class RootUtils {
         ExecCommand cmd = new ExecCommand();
         try {
             makeSystemRWState(true);
-            cmd.execute("cp -r " + appOldDir
-                    + " " + appDir, "touch /system/addon.d/99-cmmoveapps.sh", "echo "
-                    + genScript(getFiles(appDir)) + " > /system/addon.d/99-cmmoveapps.sh",
-                    "rm -rf " + appOldDir);
+            cmd.execute("cp -r " + appOldDir + " " + appDir, "rm -rf "+appOldDir);
             if(!cmd.get().isEmpty()) Log.d("rootUtils", "Error moving to /system");
+            makeScript(getFiles(appDir), context, scriptName);
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -58,8 +58,7 @@ public class RootUtils {
         ExecCommand cmd1 = new ExecCommand();
         List<String> result = new ArrayList<>();
         try {
-            result = cmd1.execute("busybox find " + path + " -type f " +
-                    "> " + Environment.getExternalStorageDirectory() + "/.cmAppsCache").get();
+            result = cmd1.execute("busybox find " + path + " -type f ").get();
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -67,7 +66,54 @@ public class RootUtils {
         return result;
     }
 
-    private static String genScript(List<String> paths) {
+    private static String makeScript(List<String> paths, Context context, String scriptName) {
+        ExecCommand cmd = new ExecCommand();
+        StringBuilder sb1 = new StringBuilder();
+        boolean oldAndroid = true;
+        if(Build.VERSION.SDK_INT >= 21) oldAndroid = false;
+        for(String a : paths) {
+            if(oldAndroid) sb1.append(a.substring(a.indexOf("/system/priv-app")+"/system/".length()+1)).append("\n");
+            else sb1.append(a.substring(a.indexOf("/system/app")+"/system/".length()+1)).append("\n");
+        }
+        //sb1.append("/");
+        try {
+            List<String> res = cmd.execute("echo '#!/sbin/sh\n" +
+                    ". /tmp/backuptool.functions\n" +
+                    "list_files() {\n" +
+                    "cat <<EOF\n" + sb1.toString() +
+                    "EOF\n" +
+                    "}\n" +
+                    "case \"$1\" in\n" +
+                    "  backup)\n" +
+                    "    list_files | while read FILE DUMMY; do\n" +
+                    "      backup_file $S/\"$FILE\"\n" +
+                    "    done\n" +
+                    "  ;;\n" +
+                    "  restore)\n" +
+                    "    list_files | while read FILE REPLACEMENT; do\n" +
+                    "      R=\"\"\n" +
+                    "      [ -n \"$REPLACEMENT\" ] && R=\"$S/$REPLACEMENT\"\n" +
+                    "      [ -f \"$C/$S/$FILE\" ] && restore_file $S/\"$FILE\" \"$R\"\n" +
+                    "    done\n" +
+                    "  ;;\n" +
+                    "  pre-backup)\n" +
+                    "    # Stub\n" +
+                    "  ;;\n" +
+                    "  post-backup)\n" +
+                    "    # Stub\n" +
+                    "  ;;\n" +
+                    "  pre-restore)\n" +
+                    "    # Stub\n" +
+                    "  ;;\n" +
+                    "  post-restore)\n" +
+                    "    # Stub\n" +
+                    "  ;;\n" +
+                    "esac' >> /system/addon.d/99-"+ scriptName +".sh").get();
+            //List<String> res = cmd.execute("sed '1,/EOF/s//" + sb1.toString() + "\nEOF/' /system/addon.d/99-save.sh").get();
+            Log.d("rootUtils", "sed result: " + res);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
